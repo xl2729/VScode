f6f99de63b582423428273bb40d31c2cede99285
xl2729 <2729273955@qq.com>
2022-01-06 01:38:15 +0800

    泛型01作业

---
+++ Java/day17/demo1/Test.java
@@ -0,0 +1,17 @@
+package Java.day17.demo1;
+
+import java.util.HashSet;
+
+public class Test {
+    public static void main(String[] args) {
+        HashSet<String> treeSet = new HashSet<>();
+        treeSet.add("111");
+        treeSet.add("爱国");
+        treeSet.add("222");
+        treeSet.add("333");
+        treeSet.add("阿斯钢");
+        treeSet.add("444");
+        System.out.println(treeSet);
+    }
+    
+}
